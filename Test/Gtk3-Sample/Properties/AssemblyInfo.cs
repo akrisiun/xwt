08f9fc48aaa29daca545e66151359ddcbb4b4a56
AndriusK <akrisiun@gmail.com>
2015-07-11 17:17:10 +0300

    gtk3 sample compiled

--- Test/Gtk3-Sample/Properties/AssemblyInfo.cs
+++ Test/Gtk3-Sample/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("sample.Properties")]
+[assembly: AssemblyTitle("Gtk3 sample")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Mono")]
-[assembly: AssemblyProduct("sample.Properties")]
+[assembly: AssemblyProduct("Gtk3 sample")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
